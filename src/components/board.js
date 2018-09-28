17356c662efd28c13fe0c2e665a112f66847a562
rodrigowebdev <rodrigo.queiroz.chagas@gmail.com>
2018-09-28 15:55:12 -0300

    Taking turns

--- src/components/board.js
+++ src/components/board.js
@@ -3,8 +3,31 @@ import ReactDOM from 'react-dom'
 import Square from './square'
 
 class Board extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true
+    }
+  }
+
+  handleClick(i){
+    const squares = this.state.squares.slice();
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext
+    })
+  }
+
   renderSquare(i) {
-    return <Square />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() =>
+          this.handleClick(i)}
+      />
+    )
   }
 
   render() {
