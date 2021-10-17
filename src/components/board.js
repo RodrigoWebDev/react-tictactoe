5162a2ffdfccbc72eaeb102c4b113cafacf4f8de
Rodrigo Queiroz <rodrigo.queiroz.chagas@gmail.com>
2021-10-17 11:37:22 -0300

    refactor to hooks

--- src/components/board.js
+++ src/components/board.js
@@ -1,99 +1,90 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import Square from './square'
 
-class Board extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true
-    }
-  }
+const Board = () => {
+  const [squares, seSquares] = useState([])
+  const [xIsNext, setXIsNext] = useState(true)
+  const [status, setStatus] = useState("")
+  const [gameOver, setGameOver] = useState(false)
 
-  handleClick(i){
-    const squares = this.state.squares.slice();
-    if (calculateWinner(squares) || squares[i]){
-      return;
-    }
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({
-      squares: squares,
-      xIsNext: !this.state.xIsNext
-    })
-  }
-
-  renderSquare(i) {
-    return (
-      <Square
-        value={this.state.squares[i]}
-        onClick={() =>
-          this.handleClick(i)}
-      />
-    )
-  }
-
-  render() {
-    const winner = calculateWinner(this.state.squares);
-    let status;
-    if (winner) {
-      status = `Winner: ${winner}`;
-    } else {
-      status = `Turn: ${this.state.xIsNext ? 'X' : 'O'}`
-    }
-    let squares = this.state.squares;
-
-    check(squares);
-
-    function check(arr) {
-      for(var i=0; i<arr.length; i++){
-        if(typeof arr[i] != "string" && calculateWinner === null) {
-          return false;
-        }
+  const calculateWinner = (squares) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
       }
-      status = "No winner"
     }
+  }
 
-    return (
-      <div>
+  const handleClick = (i) => {
+    if(gameOver === false){
+      const tempSquares = [...squares];
+      const turn = xIsNext ? 'X' : 'O';
+
+      if(!tempSquares[i]) tempSquares[i] = turn;
+      
+      seSquares(tempSquares)
+      setXIsNext(!xIsNext)
+
+      if (calculateWinner(tempSquares)){
+        console.log("xIsNext", xIsNext)
+        setStatus(`Winner: ${turn}`)
+        setGameOver(true)
+      }
+    }
+  }
+
+  const renderSquare = (i) =>
+      <Square
+        value={squares[i]}
+        onClick={() =>
+          handleClick(i)}
+      />
+
+  const check = (arr) => {
+    for(var i=0; i<arr.length; i++){
+      if(typeof arr[i] != "string" && calculateWinner === null) {
+        return false;
+      }
+    }
+    setStatus("No winner")
+  }
+
+  useEffect(() => {
+    seSquares(Array(9).fill(null))
+    console.log("squares", squares)
+  }, [])
+
+  return (
+    <div>
         <div className="status">{status}</div>
         <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
+          {renderSquare(0)}
+          {renderSquare(1)}
+          {renderSquare(2)}
         </div>
         <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
+          {renderSquare(3)}
+          {renderSquare(4)}
+          {renderSquare(5)}
         </div>
         <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
+          {renderSquare(6)}
+          {renderSquare(7)}
+          {renderSquare(8)}
         </div>
       </div>
-    );
-  }
-}
-
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
+  )
 }
 
 export default Board
--- src/components/game.js
+++ src/components/game.js
@@ -1,17 +1,12 @@
 import React from 'react'
 import Board from './board'
 
-class Game extends React.Component {
-  render() {
-    return (
-      <div className="game">
-        <h1 className="game__title">Tic Tac Toe Game</h1>
-        <div className="game-board">
-          <Board />
-        </div>
-      </div>
-    );
-  }
-}
+const Game = () => 
+  <div className="game">
+    <h1 className="game__title">Tic Tac Toe Game</h1>
+    <div className="game-board">
+      <Board />
+    </div>
+  </div>
 
 export default Game
